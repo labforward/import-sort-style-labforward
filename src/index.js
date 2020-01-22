02192d9e661cb7ef623506a3300769f088978feb
Ignatius Reza <lyoneil.de.sire@gmail.com>
2020-01-22 17:37:46 +0900

    [CUB-1780] Custom import-sort-style
    The style are based on import-sort-style-module,
    which the following exceptions:
    
    - `react`, `react-redux`, `redux`, and `prop-types` are promoted
      to show up first at that specific orders
    
    - modules coming from `node_modules` are grouped separately
      from our own internal modules, to highlight which imports
      are under our control, and which are 3rd parties

---
+++ src/index.js
@@ -0,0 +1,94 @@
+"use strict";
+const path = require("path");
+const fs = require("fs");
+const FIXED_ORDER = ["react", "react-redux", "redux", "prop-types"];
+
+function isFixed(imported) {
+  return FIXED_ORDER.indexOf(imported.moduleName) !== -1;
+}
+
+exports.default = importSort;
+function importSort(styleApi) {
+  const {
+    alias,
+    and,
+    dotSegmentCount,
+    hasNoMember,
+    isAbsoluteModule,
+    isNodeModule,
+    isRelativeModule,
+    moduleName,
+    naturally,
+    unicode
+  } = styleApi;
+
+  const nodeModules = path.resolve(__dirname, "../../../node_modules");
+  const modules = fs.readdirSync(nodeModules);
+
+  function isFromNodeModules(imported) {
+    return (
+      modules.indexOf(imported.moduleName.split("/")[0]) !== -1 && // packages coming in from 'node_modules'
+      !imported.moduleName.startsWith("react/") // components which we wrote but is under react namespace
+    );
+  }
+
+  function fixedComparator(module1, module2) {
+    const i1 = FIXED_ORDER.indexOf(module1);
+    const i2 = FIXED_ORDER.indexOf(module2);
+
+    i1 = i1 === -1 ? Number.MAX_SAFE_INTEGER : i1;
+    i2 = i2 === -1 ? Number.MAX_SAFE_INTEGER : i2;
+
+    return i1 === i2 ? naturally(module1, module2) : i1 - i2;
+  }
+
+  return [
+    // import "foo"
+    { match: and(hasNoMember, isAbsoluteModule) },
+    { separator: true },
+
+    // import "./foo"
+    { match: and(hasNoMember, isRelativeModule) },
+    { separator: true },
+
+    // import … from "fs"; # native node modules
+    {
+      match: isNodeModule,
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode)
+    },
+    { separator: true },
+
+    // module which have fixed position (see FIXED_ORDER)
+    {
+      match: isFixed,
+      sort: moduleName(fixedComparator),
+      sortNamedMembers: alias(unicode)
+    },
+
+    // import uniq from 'lodash/uniq'; # modules from node_modules directory
+    {
+      match: isFromNodeModules,
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode)
+    },
+    { separator: true },
+
+    // import … from "foo"; # modules with absolute path
+    {
+      match: isAbsoluteModule,
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode)
+    },
+    { separator: true },
+
+    // import … from "./foo"; # modules with relative path
+    // import … from "../foo";
+    {
+      match: isRelativeModule,
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode)
+    },
+    { separator: true }
+  ];
+}
